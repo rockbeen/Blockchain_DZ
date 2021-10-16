0cbddbe0bd61bb0d11d92eb66f7270e3095e022f
rockbeen <62306907+rockbeen@users.noreply.github.com>
2021-10-16 18:42:37 +0300

    Add files via upload

---
+++ dz_2/dz_2.sol
@@ -0,0 +1,25 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract multiplication {
+
+	
+	uint8 public result = 1;
+
+	constructor() public {
+	
+		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	
+
+
+	function multiply(uint8 value) public  {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        require(value>=1 && value<=10, 103);//checking the boundaries of a number
+        tvm.accept();
+		result *= value;
+	}
+}
